a71f86b50fd56b310b24097de92a96d9efdf69a0
Kianoush Haratiannejadi <haratiank2@gmail.com>
2023-12-07 09:26:26 -0500

    encoding data

--- Medical EDA.py
+++ Medical EDA.py
@@ -19,16 +19,31 @@ import matplotlib.pyplot as pl
 import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
-
+from sklearn.preprocessing import LabelEncoder
 
 data = pd.read_csv('data/insurance.csv')
 
 head = data.head()
 
+data.isnull().sum()
 
 
+# =============================================================================
+# PART 1: data encoding
+# =============================================================================
 
+#sex
+le = LabelEncoder()
+le.fit(data.sex.drop_duplicates()) 
+data.sex = le.transform(data.sex)
 
+# smoker or not
+le.fit(data.smoker.drop_duplicates()) 
+data.smoker = le.transform(data.smoker)
+
+#region
+le.fit(data.region.drop_duplicates()) 
+data.region = le.transform(data.region)
 
 
 
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Medical Cost Data Analysis-EDA
 Today we will explore a data set dedicated to the cost of treatment of different patients. The cost of treatment depends on many factors: diagnosis, type of clinic, city of residence, age and so on. We have no data on the diagnosis of patients. But we have other information that can help us to make a conclusion about the health of patients and practice regression analysis. In any case, I wish you to be healthy! Let's look at our data.
 
+# Steps
+* Use Data LabelEncoder on relevant coulmn like gender, smoker, etc.
 
 ## Visualizition Analysis
 
